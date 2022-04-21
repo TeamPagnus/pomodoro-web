30e2f8c1fa372b2bc9ac28dc5eacbd29d436d0b0
absentabyss <agna.lumi@protonmail.com>
2022-04-21 15:46:47 -0300

    *

---
+++ script.js
@@ -0,0 +1,42 @@
+const fullPeriod = 70 * 60;
+const workPeriod = 55 * 60;
+
+
+function sleep(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+function isWorkingState(seconds) {
+    return seconds % fullPeriod < workPeriod;
+}
+
+function convertToClock(seconds) {
+    return new Date(seconds * 1000).toISOString().substr(11, 8);
+}
+
+async function main() {
+    var timer = document.getElementById("timer");
+    var seconds;
+    var secondsLeft;
+    var audio = new Audio('chime.ogg');
+    while (true) {
+        timestamp = +new Date();
+        seconds = Math.floor(timestamp / 1000);
+        if (isWorkingState(seconds)) {
+            secondsLeft = workPeriod - (seconds % fullPeriod);
+            if (secondsLeft == 0) {
+                audio.play();
+            }
+            timer.innerHTML = convertToClock(secondsLeft) + " Work";
+        } else {
+            secondsLeft = fullPeriod - (seconds % fullPeriod);
+            if (secondsLeft == 0) {
+                audio.play();
+            }
+            timer.innerHTML = convertToClock(secondsLeft) + " Break";
+        }
+        await sleep(1000);
+    }
+}
+
+main();
